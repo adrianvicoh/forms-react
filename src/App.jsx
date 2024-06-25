9852cf321513d485c5216bf92dc6fb1bb761ca4a
adrianvicoh <1hiaw.vico.adrian@gmail.com>
2024-06-25 17:07:11 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Hola que tal</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/prueba.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../../calculadora-react/src/App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <p>Adiós</p>
+  </React.StrictMode>,
+)
