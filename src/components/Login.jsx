9eff901f5dbadba207a0baf8cabd9952fe52e9fe
adrianvicoh <1hiaw.vico.adrian@gmail.com>
2024-06-27 16:57:53 +0200

    Hecha funcionalidad de coores de login en 10b forms

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,26 +3,59 @@ import { Container, Form, Row, Col, Button } from "react-bootstrap";
 
 function Login(props) {
 
-    const nombre = props.nombre_correcto;
-    const password = props.password_correcto;
+    const correctName = props.nombre_correcto;
+    const correctPassword = props.password_correcto;
+
+    const [isNameVerified, setNameVerified] = useState(false);
+    const [isPasswordVerified, setPasswordVerified] = useState(false);
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+    let styleRed = {
+        color:'red'
+    }
+
+    let styleGreen = {
+        color:'green'
+    }
+
+    function handleInput(input) {
+        console.log(input.target.value)
+        if (input.target.name === "name") {
+            setName(input.target.value);
+        } else {
+            setPassword(input.target.value);
+        }
+    }
+
+    function verificar() {
+        input.preventDefault();
+        
+        if (name === correctName) {
+            setNameVerified(true)
+        }
+        if (password === correctPassword) {
+            setPasswordVerified(true)
+        }
+    }
 
     return (
         <>
             <Container>
                 <Row className="justify-content-md-center">
                     <Col xs lg="6">
-                        <Form>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Nombre</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
+                    <h1 className='text-center m-5'>Login</h1>
+                        <Form onSubmit={verificar}>
+                            <Form.Group className="mb-3" name="name" onChange={handleInput}>
+                                <Form.Label style={isNameVerified ? styleGreen : styleRed}>Nombre</Form.Label>
+                                <Form.Control type="text" placeholder="Nombre" />
                             </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
+                            <Form.Group className="mb-3" name="password" onChange={handleInput}>
+                                <Form.Label style={isPasswordVerified ? styleGreen : styleRed}>Password</Form.Label>
                                 <Form.Control type="password" placeholder="Password" />
                             </Form.Group>
                             <Button variant="primary" type="submit">
-                                Submit
+                                Login
                             </Button>
                         </Form>
                     </Col>
